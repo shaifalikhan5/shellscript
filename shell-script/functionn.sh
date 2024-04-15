c16564b5b9f10f68f92127f5666f5f61ffdc7f9e
shaifalikhan <shaifalikhan5@gmail.com>
2024-04-15 10:14:32 +0530

    added logfile creation

--- shell-script/functionn.sh
+++ shell-script/functionn.sh
@@ -1,3 +1,27 @@
-#!bin/bash
+#!/bin/bash
+# define variable
 DATE=$(date +%F-%H-%M-%S)
-LOGFILE=/tmp/
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then 
+     echo "error : please run the script with root access"
+     exit 1
+else 
+    echo "you are root user"
+fi
+apt install mysql-server -y &>$LOGFILE
+if [ $? -ne 0 ]
+  then
+      echo "installation of mysql failed"
+      exit 1
+  else
+     echo "instalation of mysql success"
+fi         
+      
+
+
+
